42c3fc16a461f061580ec9ab85439d994e331555
RafiulIslamJim <rijim39@gmail.com>
2024-09-11 20:48:08 +0600

    again changes

--- dist/first.js
+++ dist/first.js
@@ -1,6 +1,18 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales = 134545;
+let course = 'TypeScript';
+let isPublished = true;
+function greet(name) {
+    return 'Hello ' + name;
+}
+console.log(greet(("Jim")));
+console.log(sales);
+console.log(course);
+console.log(isPublished);
+let numbers = [2, 3, 57, 75];
+numbers.push(5);
+let lastNumber = numbers.pop();
+console.log(lastNumber);
+let doubledNumbers = numbers.map(num => num * 2);
+console.log(doubledNumbers);
 //# sourceMappingURL=first.js.map
--- src/first.ts
+++ src/first.ts
@@ -1,5 +1,24 @@
-let age: number = 20;
-if(age < 50)
-  age+=10;
+let sales: number = 134545;
+let course: string = 'TypeScript';
+let isPublished: boolean = true;
 
-console.log(age);
+function greet(name: string): string
+{
+  return 'Hello ' + name;
+}
+
+console.log(greet(("Jim")));
+console.log(sales);
+console.log(course);
+console.log(isPublished);
+
+let numbers: number[] = [2,3,57,75];
+
+numbers.push(5);
+let lastNumber = numbers.pop();
+console.log(lastNumber);
+
+let doubledNumbers = numbers.map(num => num*2);
+console.log(doubledNumbers);
+
+
